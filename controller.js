94cc3b51b5ad03111225afe26bd47d1d87e32f91
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-08-31 13:38:24 -0600

    finished project

--- controller.js
+++ controller.js
@@ -1,10 +1,34 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+  $scope.pageNum = 1;
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
-
-
+    mainService.getUsers($scope.pageNum).then(function(response) {
+      $scope.users = response.data.data;
+      $scope.total_pages = response.data.total_pages;
+    });
+  };
   $scope.getUsers();
 
+  $scope.previous = function() {
+    if($scope.pageNum > 1) {
+      $scope.pageNum--;
+      $scope.getUsers();
+  }
+  };
+  $scope.next = function() {
+    if ($scope.total_pages > $scope.pageNum) {
+      $scope.pageNum++;
+      $scope.getUsers();
+    }
+  };
+
 });
+
+
+// The way Jeremy did it:
+//
+// var promise = mainService.getUsers();
+//
+// promise.then(function(response) {
+//      $scope.users = response.data.data;
+// });
